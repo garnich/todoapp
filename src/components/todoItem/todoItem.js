606b0a797dc3c4e95b1f45ed515c3779803d5b2c
garnich <garnich@tut.by>
2019-11-28 22:28:55 +0300

    Add proptypes & pre-commit hook

--- src/components/todoItem/todoItem.js
+++ src/components/todoItem/todoItem.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
-import './todoItem.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const TodoItem = (props) => {   
-        const {
-            name, 
-            delItem, 
-            addToDone, 
-            addToImportant, 
-            done, 
-            important} = props;
-        let classNames = '';
-        if(done) classNames += ' done';
-        if(important) classNames += ' important';
+import './todoItem.css'
 
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span
-                onClick={ addToDone }
-                className={ classNames }
-            >{name}</span>
-            <span>
-                <button
-                    className="btn btn-outline-success"
-                    onClick={addToImportant}
-                >
-                    <i className="fa fa-flag" />
-                </button>
-                <button
-                    className="btn btn-outline-danger"
-                    onClick={delItem}
-                >
-                    <i className="fa fa-trash-o" />
-                </button>
-            </span>
-        </li>
-    )    
-};
+const TodoItem = props => {
+  const { name, delItem, addToDone, addToImportant, done, important } = props
+  let classNames = ''
+  if (done) classNames += ' done'
+  if (important) classNames += ' important'
 
-export default TodoItem;
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <span onClick={addToDone} className={classNames}>
+        {name}
+      </span>
+      <span>
+        <button className="btn btn-outline-success" onClick={addToImportant}>
+          <i className="fa fa-flag" />
+        </button>
+        <button className="btn btn-outline-danger" onClick={delItem}>
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  delItem: PropTypes.func.isRequired,
+  addToDone: PropTypes.func.isRequired,
+  addToImportant: PropTypes.func.isRequired,
+  done: PropTypes.bool,
+  important: PropTypes.bool,
+}
+
+TodoItem.defaultProps = {
+  done: false,
+  important: false,
+}
+
+export default TodoItem
