9b24d48c5cf0494bac0b33727cd08c832481589d
garnich <garnich@tut.by>
2020-04-13 22:50:38 +0300

    fix header navigation logic and styles

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
-import {
-  FaNpm,
-  FaReact,
-  FaNode,
-  FaSass,
-  FaBootstrap,
-  FaDatabase,
-} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { FaNpm, FaReact, FaNode, FaSass, FaBootstrap } from 'react-icons/fa'
 import { DiFirebase } from 'react-icons/di'
 
 import './homePage.scss'
@@ -26,14 +20,20 @@ class HomePage extends Component {
       <div className="col-12 home_page">
         <p>{`Hi, today is ${day} of ${month}, ${year} year.`}</p>
         <p>This is one of my app - ToDo.</p>
-        <p className="reactIcons">
-          <FaNpm />
-          <FaReact />
-          <FaNode />
-          <FaSass />
-          <FaBootstrap />
-          <DiFirebase />
-        </p>
+        <Link to="/todo">
+          <button className="btn btn-outline-secondary btn-lg mt-4 mb-4">
+            {' '}
+            Welcome!{' '}
+          </button>
+        </Link>
+        <div className="reactIcons">
+          <FaNpm title="NPM" />
+          <FaReact title="React" />
+          <FaNode title="Node" />
+          <FaSass title="Sass" />
+          <FaBootstrap title="Bootstrap" />
+          <DiFirebase title="Firebase" />
+        </div>
       </div>
     )
   }
